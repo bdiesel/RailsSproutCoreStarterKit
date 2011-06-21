479d8fd9419a40da83c77d080c4f7ae13abc9eb3
Brian Diesel <bri@ndiesel.net>
2011-06-21 15:34:41 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSproutCoreKit::Application.config.secret_token = '0079891fe82e0867c74b6c0638d8dea840f0b5ea7c1e14b0863996060b56b74151a2464005aedce8c1fb8d43612354a810b5f28c4885de9554738be9f7068241'
